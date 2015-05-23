af51e74cf4da742d5b567045451ddd50f56bccfb
caidui <checaide@outlook.com>
2015-05-23 15:53:54 +0800

    程序补丁
    虚假警告补丁

--- base/workers.c
+++ base/workers.c
@@ -290,6 +290,8 @@ static void destroy_job(struct wproc_job *job)
 	case WPJOB_SVC_EVTHANDLER:
 	case WPJOB_GLOBAL_HOST_EVTHANDLER:
 	case WPJOB_HOST_EVTHANDLER:
+	case WPJOB_HOST_PERFDATA:
+	case WPJOB_SVC_PERFDATA:
 		/* these require nothing special */
 		break;
 	case WPJOB_CALLBACK:
@@ -772,6 +774,11 @@ static int handle_worker_result(int sd, int events, void *arg)
 		case WPJOB_CALLBACK:
 			run_job_callback(job, &wpres, 0);
 			break;
+
+        case WPJOB_HOST_PERFDATA:
+        case WPJOB_SVC_PERFDATA:
+            /* these require nothing special */
+            break;
 
 		default:
 			logit(NSLOG_RUNTIME_WARNING, TRUE, "Worker %d: Unknown jobtype: %d\n", wp->pid, job->type);
